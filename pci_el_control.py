6aa629da34d044c6af8cb5e8d3215875d1ebc101
SimMarkt <s.markt@posteo.de>
2024-12-20 10:20:25 +0100

    Update 20122024

---
+++ pci_el_control.py
@@ -0,0 +1,19 @@
+# ----------------------------------------------------------------------------------------------------------------
+# PyComInt: Communication interface of chemical plants
+# https://github.com/SimMarkt/PyComInt
+
+# pci_el_control.py: 
+# > implements the modbus connection to the PEMEL and converts the set value from a OPCUA node in the PLC of the BM to the Modbus client# 
+# ----------------------------------------------------------------------------------------------------------------
+
+import opcua
+import pymodbus
+import yaml
+
+def data_trans_func2():
+    return print(f"Data transfer2 successfull!")
+
+def el_control_func():
+    h2_flow_rate = 10
+    el_current = 2
+    return print(f"PEMEL control successfull: {h2_flow_rate} Nl/min -> {el_current} A")
